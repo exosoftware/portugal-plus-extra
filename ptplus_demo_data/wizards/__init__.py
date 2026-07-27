6bf2bbc1ab3c875e24e29c3fd2252026a4825ecb
diogop-exo <diogo.pereira@exo.pt>
2026-07-27 12:39:17 +0100

    [19.0][REM] removed ptplus_demo_data module

--- ptplus_demo_data/wizards/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import ptplus_demo_data_wizard
-from . import demo_fiscal_documents
-from . import demo_sales_crm
-from . import demo_project
-from . import demo_hr
-from . import demo_marketing
-from . import demo_accounting
